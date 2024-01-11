6f0ce0eb9502313e6a3dcaa9bb2e4cf29059b989
Matchobas <maalexandria@hotmail.com>
2024-01-10 21:46:58 -0300

    chore: learning more about custom streams

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import { stdin, stdout } from 'node:process';
+import { Writable } from 'node:stream';
 
-stdin.pipe(stdout);
+class MyWriter extends Writable {
+  _write(chunk: Buffer, encoding: BufferEncoding, callback: Function) {
+    process.stdout.write(`Hey, I wrote this:  ${chunk.toString('utf8')}`);
+    callback();
+  }
+}
+
+const myWriter = new MyWriter();
+
+stdin.pipe(myWriter);
