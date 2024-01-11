0e8126369de7659261483f715afbcfeac0dba442
Matchobas <maalexandria@hotmail.com>
2024-01-11 09:31:35 -0300

    chore: doing some challenges with streams

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { stdin, stdout } from 'node:process';
 import { Writable } from 'node:stream';
 
 class MyWriter extends Writable {
+  backwards(text: string): string {
+    let back = '';
+    for(let i = text.length - 1; i >= 0; i--) {
+      back += text[i];
+    }
+    return back;
+  }
   _write(chunk: Buffer, encoding: BufferEncoding, callback: Function) {
-    process.stdout.write(`Hey, I wrote this:  ${chunk.toString('utf8')}`);
+    const backwards = this.backwards(chunk.toString('utf8'));
+    process.stdout.write(`${backwards} \n`);
     callback();
   }
 }
