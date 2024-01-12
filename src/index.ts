d27854fb1ee572efae743b91df9c8848f2a2b253
Matchobas <maalexandria@hotmail.com>
2024-01-12 20:38:00 -0300

    feat: creating first socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import * as net from 'node:net';
+import * as net from "node:net";
 
 function newConn(socket: net.Socket) {
-  console.log(`new connection ${socket.remoteAddress} // ${socket.remotePort}`)
+  console.log(`new connection ${socket.remoteAddress} // ${socket.remotePort}`);
+
+  socket.on("end", () => {
+    console.log("EOF");
+  });
+
+  socket.on("data", (data: Buffer) => {
+    console.log("data: ", data);
+    socket.write(data);
+
+    if (data.includes("q")) {
+      console.log("closing.");
+      socket.end();
+    }
+  });
 }
 
 const socket = net.createServer();
-socket.on('connection', newConn);
 
-socket.listen({ port: 1234, host: '127.0.0.1'});
+socket.on("error", (err) => {
+  throw err.message;
+});
+socket.on("connection", newConn);
 
+socket.listen({ port: 1234, host: "127.0.0.1" });
