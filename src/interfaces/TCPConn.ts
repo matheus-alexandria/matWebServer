1434ee2451e488e66d494abafd3e0d3cfb469d81
Matchobas <maalexandria@hotmail.com>
2024-01-18 20:29:17 -0300

    feat: add properties for end and error socket actions

--- src/interfaces/TCPConn.ts
+++ src/interfaces/TCPConn.ts
@@ -2,6 +2,8 @@ import { Socket } from "node:net";
 
 export interface TCPConn {
   socket: Socket;
+  ended: boolean;
+  err: null | Error;
   reader: null | {
     resolve: (value: Buffer) => void,
     reject: (reason: Error) => void
